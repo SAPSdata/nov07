2cc48e0ec066bb0bf04f2531e059c8bd7ae69e1b
SAPSdata <88047600+SAPSdata@users.noreply.github.com>
2021-11-07 00:24:18 +0000

    Update settings.py

--- zoopla/settings.py
+++ zoopla/settings.py
@@ -7,7 +7,7 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'nov07'
+BOT_NAME = 'zoopla'
 
 SPIDER_MODULES = ['zoopla.spiders']
 NEWSPIDER_MODULE = 'zoopla.spiders'
